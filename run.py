7567f2fa3e372078607ad5964fa9411a8a910fe1
georgegach <george@gach.space>
2018-10-11 10:41:55 -0400

    default param

--- run.py
+++ run.py
@@ -11,8 +11,8 @@ if __name__ == '__main__' :
 
     parser = ArgumentParser()
     parser.add_argument('-i', '--input-dir', dest="input", help="Specify the input directory", type=str, required=True)
-    parser.add_argument('-ow', '--output-width', dest="width", help="Specify the output file width. Default 300", type=int)
-    parser.add_argument('-oh', '--output-height', dest="height", help="Specify the output file height. Default 400", type=int)
+    parser.add_argument('-ow', '--output-width', dest="width", help="Specify the output file width. Default 300", type=int, default=300)
+    parser.add_argument('-oh', '--output-height', dest="height", help="Specify the output file height. Default 400", type=int, default=400)
     parser.add_argument('-e', '--extensions', dest="ext", help="Specify the file extensions like *.jpg *.whatevs.file", nargs="+")
     parser.add_argument('-o', '--output-path', dest="output", help="Specify the output path for writing result. Default ./results/[input-dir-names].jpg", type=str)
     parser.add_argument('-w', '--window', dest="window", help="Shows window if specified", action="store_true", default=False)
